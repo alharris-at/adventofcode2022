633d6a183da61f4ac9febf3524f656be8d7f9305
Alexander Harris <alharris@amazon.com>
2022-12-05 17:12:20 -0800

    adding day 5 solutions

--- day5_1/src/main.rs
+++ day5_1/src/main.rs
@@ -1,44 +1,114 @@
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::{BufRead, Lines, BufReader, Result};
 use std::path::Path;
 use regex::Regex;
 
 #[derive(Debug)]
-struct LineData {
-    first_min: i32,
-    first_max: i32,
-    second_min: i32,
-    second_max: i32,
+struct Move {
+    source_stack_index: usize,
+    destination_stack_index: usize,
+    move_count: u32,
 }
 
 fn main() {
-    let mut score: u32 = 0;
+    let moves = construct_moves();
+    println!("Generated moves {:?}", moves);
 
-    // File hosts must exist in current path before this produces output
-    if let Ok(lines) = read_lines("src/input.txt") {
-        // Consumes the iterator, returns an (Optional) String
+    let mut initial_stacks = construct_stack_state();
+    println!("Generated initial stack state {:?}", initial_stacks);
+
+    let modified_stacks = process_stack_moves(&mut initial_stacks, moves);
+    println!("Generated modified stack state {:?}", modified_stacks);
+
+    let top_values = get_top_values(modified_stacks);
+    println!("Top Values: {:?}", top_values);
+
+    let response_string = String::from_iter(top_values);
+    println!("Result {}", response_string);
+}
+
+fn get_top_values(stack_state: &mut Vec<VecDeque<char>>) -> Vec<char> {
+    let mut top_values = Vec::new();
+
+    for stack in stack_state {
+        top_values.push(stack.pop_front().unwrap());
+    }
+
+    return top_values;
+}
+
+fn process_stack_moves(stack_state: &mut Vec<VecDeque<char>>, moves: Vec<Move>) -> &mut Vec<VecDeque<char>> {
+    for move_config in moves {
+        let mut temp_queue: Vec<char> = Vec::new();
+        {
+            let source_stack = stack_state.get_mut(move_config.source_stack_index - 1).unwrap();
+            for _ in 0..move_config.move_count {
+                let moved_char = source_stack.pop_front().unwrap();
+                temp_queue.push(moved_char);
+            }
+        }
+        {
+            let destination_stack = stack_state.get_mut(move_config.destination_stack_index - 1).unwrap();
+            for char_to_push in temp_queue {
+                destination_stack.push_front(char_to_push)
+            }
+        }
+    }
+    return stack_state;
+}
+
+fn construct_moves() -> Vec<Move> {
+    let mut moves = Vec::new();
+
+    let re = Regex::new(r"^move (\d*) from (\d*) to (\d*)$").unwrap();
+
+    if let Ok(lines) = read_lines("src/moves.txt") {
         for line in lines {
             if let Ok(val) = line {
-                let line_data = line_to_data(&val);
-                if line_data.first_min <= line_data.second_max && line_data.first_max >= line_data.second_min {
-                    score += 1;
+                let cap = re.captures(&val).unwrap();
+                moves.push(Move {
+                    source_stack_index: cap[2].parse::<usize>().unwrap(),
+                    destination_stack_index: cap[3].parse::<usize>().unwrap(),
+                    move_count: cap[1].parse::<u32>().unwrap(),
+                });
+            }
+        }
+    }
+
+    return moves;
+}
+
+fn construct_stack_state() -> Vec<VecDeque<char>> {
+    let mut stacks: Option<Vec<VecDeque<char>>> = None;
+    let mut stack_count: Option<usize> = None;
+
+    if let Ok(lines) = read_lines("src/state.txt") {
+        for line in lines {
+            if let Ok(val) = line {
+                if let None = stacks {
+                    stack_count = Some((val.chars().count() + 1) / 4);
+                    let mut local_stacks: Vec<VecDeque<char>> = Vec::new();
+                    for _ in 0..stack_count.unwrap() {
+                        local_stacks.push(VecDeque::new());
+                    }
+                    stacks = Some(local_stacks);
+                }
+                
+                if let Some(stacks) = &mut stacks {
+                    let row_chars: Vec<char> = val.chars().collect();
+                    for stack_index in 0..stack_count.unwrap() {
+                        let stack_index_char = row_chars.get(stack_index * 4 + 1).unwrap().clone();
+                        if stack_index_char != ' ' {
+                            stacks.get_mut(stack_index).unwrap().push_back(stack_index_char);
+                        }
+                    }
                 }
             }
         }
     }
 
-    println!("Got score {}", score);
-}
-
-fn line_to_data(line: &String) -> LineData {
-    let re = Regex::new(r"^(\d*)-(\d*),(\d*)-(\d*)$").unwrap();
-    let cap = re.captures(line).unwrap();
-    return LineData {
-        first_min: cap[1].parse::<i32>().unwrap(),
-        first_max: cap[2].parse::<i32>().unwrap(),
-        second_min: cap[3].parse::<i32>().unwrap(),
-        second_max: cap[4].parse::<i32>().unwrap(),
-    };
+    return stacks.unwrap();
 }
 
 // The output is wrapped in a Result to allow matching on errors
--- day5_2/src/main.rs
+++ day5_2/src/main.rs
@@ -1,3 +1,120 @@
+use std::collections::VecDeque;
+use std::fs::File;
+use std::io::{BufRead, Lines, BufReader, Result};
+use std::path::Path;
+use regex::Regex;
+
+#[derive(Debug)]
+struct Move {
+    source_stack_index: usize,
+    destination_stack_index: usize,
+    move_count: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let moves = construct_moves();
+    println!("Generated moves {:?}", moves);
+
+    let mut initial_stacks = construct_stack_state();
+    println!("Generated initial stack state {:?}", initial_stacks);
+
+    let modified_stacks = process_stack_moves(&mut initial_stacks, moves);
+    println!("Generated modified stack state {:?}", modified_stacks);
+
+    let top_values = get_top_values(modified_stacks);
+    println!("Top Values: {:?}", top_values);
+
+    let response_string = String::from_iter(top_values);
+    println!("Result {}", response_string);
+}
+
+fn get_top_values(stack_state: &mut Vec<VecDeque<char>>) -> Vec<char> {
+    let mut top_values = Vec::new();
+
+    for stack in stack_state {
+        top_values.push(stack.pop_front().unwrap());
+    }
+
+    return top_values;
+}
+
+fn process_stack_moves(stack_state: &mut Vec<VecDeque<char>>, moves: Vec<Move>) -> &mut Vec<VecDeque<char>> {
+    for move_config in moves {
+        let mut temp_queue: VecDeque<char> = VecDeque::new();
+        {
+            let source_stack = stack_state.get_mut(move_config.source_stack_index - 1).unwrap();
+            for _ in 0..move_config.move_count {
+                let moved_char = source_stack.pop_front().unwrap();
+                temp_queue.push_front(moved_char);
+            }
+        }
+        {
+            let destination_stack = stack_state.get_mut(move_config.destination_stack_index - 1).unwrap();
+            for char_to_push in temp_queue {
+                destination_stack.push_front(char_to_push)
+            }
+        }
+    }
+    return stack_state;
+}
+
+fn construct_moves() -> Vec<Move> {
+    let mut moves = Vec::new();
+
+    let re = Regex::new(r"^move (\d*) from (\d*) to (\d*)$").unwrap();
+
+    if let Ok(lines) = read_lines("src/moves.txt") {
+        for line in lines {
+            if let Ok(val) = line {
+                let cap = re.captures(&val).unwrap();
+                moves.push(Move {
+                    source_stack_index: cap[2].parse::<usize>().unwrap(),
+                    destination_stack_index: cap[3].parse::<usize>().unwrap(),
+                    move_count: cap[1].parse::<u32>().unwrap(),
+                });
+            }
+        }
+    }
+
+    return moves;
+}
+
+fn construct_stack_state() -> Vec<VecDeque<char>> {
+    let mut stacks: Option<Vec<VecDeque<char>>> = None;
+    let mut stack_count: Option<usize> = None;
+
+    if let Ok(lines) = read_lines("src/state.txt") {
+        for line in lines {
+            if let Ok(val) = line {
+                if let None = stacks {
+                    stack_count = Some((val.chars().count() + 1) / 4);
+                    let mut local_stacks: Vec<VecDeque<char>> = Vec::new();
+                    for _ in 0..stack_count.unwrap() {
+                        local_stacks.push(VecDeque::new());
+                    }
+                    stacks = Some(local_stacks);
+                }
+                
+                if let Some(stacks) = &mut stacks {
+                    let row_chars: Vec<char> = val.chars().collect();
+                    for stack_index in 0..stack_count.unwrap() {
+                        let stack_index_char = row_chars.get(stack_index * 4 + 1).unwrap().clone();
+                        if stack_index_char != ' ' {
+                            stacks.get_mut(stack_index).unwrap().push_back(stack_index_char);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return stacks.unwrap();
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> Result<Lines<BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(BufReader::new(file).lines())
 }
